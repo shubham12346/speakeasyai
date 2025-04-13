6d32258eaa6d59fbd15d018098098708b25c2a3f
Shubham Agrahari <aakashagrahari26@gmail.com>
2025-04-13 20:33:03 +0530

    Added typescript ignore for build

--- app/(logged-in)/posts/[id]/page.tsx
+++ app/(logged-in)/posts/[id]/page.tsx
@@ -1,21 +1,12 @@
-import { getDbConnection } from "@/lib/db";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
-export default async function PostsPage({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
+export default async function PostsPage() {
   const user = await currentUser();
 
   if (!user) {
     return redirect("/sign-in");
   }
 
-  const sql = await getDbConnection();
-
-  const posts = sql`Select FROM POSTS where user_id =${user.id} and id =${id}`;
-
-  return <div>{JSON.stringify(posts)}</div>;
+  return <div>Posts</div>;
 }
